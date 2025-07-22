203ecd13aa9e7ad4906e971b82613595f2212329
Niveditha Reddy Anumandla <nivi.anr1707@gmail.com>
2025-07-22 11:48:39 +0300

    Update LoginController.php

--- fs_portal_l8/app/Http/Controllers/LoginController.php
+++ fs_portal_l8/app/Http/Controllers/LoginController.php
@@ -1,29 +1,23 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 class LoginController extends Controller
 {
     public function showLoginForm()
     {
         return view('login');
     }
-
     public function login(Request $request)
     {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
-
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             return redirect()->intended('/dashboard')->with('success', 'Password correct');
         }
-
         return back()->with('error', 'Invalid credentials');
     }
 }
